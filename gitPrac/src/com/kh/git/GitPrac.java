c6f8e4a89b1cd453fe8f8cc3d9f73273b0465f12
user1 <user1@DESKTOP-JK7CNVN>
2020-04-24 11:11:40 +0900

    second_commit

--- gitPrac/src/com/kh/git/GitPrac.java
+++ gitPrac/src/com/kh/git/GitPrac.java
@@ -4,6 +4,8 @@ public class GitPrac {
 	
 	public static void main(String[] args) {
 		 System.out.println("hello");
+		 System.out.println("world");
+		 System.out.println("whatsup!!!!!!");
 	 }
 
 }
